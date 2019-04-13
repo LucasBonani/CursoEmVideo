a5e92577493d277992373646cc10e2bafb5d89da
LucasBonani <lucas.bonani@gmail.com>
2019-04-13 17:57:03 -0300

    atualização aula 11

---
+++ aulas/mundo_1/aula_11.py
@@ -0,0 +1,20 @@
+# para cores
+
+# \cod[style;text;backm
+# \033[0;33;44m
+
+
+# style                 text                        back (fundo)
+#                       30 - white                  40
+# 0 - none              31 - red                    41
+# 1 - bold              32 - green                  42
+# 4 - underline         33 - yellow                 43
+# 7 - negative          34 - blue                   44
+#                       35 - purple                 45
+#                       36 - green light            46
+#                       37 - grey                   47
+
+print('\033[1;30;41mOla mundo\033[m')
+print('\033[4;33;44mOla mundo\033[m')
+print('\033[0;35;43mOlá mundo\033[m')
+print('\033[1;30;41mOla mundo\033[m')
